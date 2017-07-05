5a8e5b4f3bbe98d48d324b3fca1a3a26fd472583
Hoang Phan <hoangds121@gmail.com>
2017-07-05 19:41:41 +0700

    milestone 1

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,23 @@
+class RoomsController < ApplicationController
+
+  def create
+    @room = Room.create(room_params)
+    if @room.save
+      flash[:success] = "#{@room.name} was successfully created..."
+      redirect_to room_path(@room)
+    else
+      flash[:error] = @room.errors.full_messages.to_sentence
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
+end
